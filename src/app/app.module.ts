379704a883a09ab055bdb2aea9165587cb023641
Shaka Blaka Zulu <liraz1234@hotmail.com>
2020-10-15 02:29:55 +0300

    added starting components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {IntroComponent} from './intro/intro.component';
+import {BioComponent} from './bio/bio.component';
+import {ResumeComponent} from './resume/resume.component';
+import {SkillsComponent} from './skills/skills.component';
+import {PortfolioComponent} from './portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IntroComponent,
+    BioComponent,
+    ResumeComponent,
+    SkillsComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/intro/intro.component.ts
@@ -0,0 +1,16 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'sbz-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
