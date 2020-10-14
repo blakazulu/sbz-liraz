514c52d43b21de58982cf9f05bb02b28edf4baf4
Shaka Blaka Zulu <liraz1234@hotmail.com>
2020-10-15 01:28:55 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sbz-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'sbz-liraz';
+}
