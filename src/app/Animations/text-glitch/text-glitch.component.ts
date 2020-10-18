1bcc1fc98ed5b2325b19e83407c776251afa312d
Shaka Blaka Zulu <liraz1234@hotmail.com>
2020-10-18 22:36:07 +0300

    added animations

--- src/app/Animations/text-glitch/text-glitch.component.ts
+++ src/app/Animations/text-glitch/text-glitch.component.ts
@@ -1,10 +1,15 @@
 import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {zoomInLeftOnEnterAnimation, zoomInRightOnEnterAnimation} from 'angular-animations';
 
 @Component({
   selector: 'sbz-text-glitch',
   templateUrl: './text-glitch.component.html',
   styleUrls: ['./text-glitch.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    zoomInLeftOnEnterAnimation({duration: 1500}),
+    zoomInRightOnEnterAnimation({duration: 1500, delay: 1000})
+  ]
 })
 export class TextGlitchComponent implements OnInit {
   constructor() {
