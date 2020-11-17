675e5f224901613619319585d938c4c93746f18f
Shaka Blaka Zulu <liraz1234@hotmail.com>
2020-11-17 16:00:29 +0200

    added google analitics

---
+++ src/app/Animations/explode/explode.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'sbz-explode',
+  templateUrl: './explode.component.html',
+  styleUrls: ['./explode.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ExplodeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
